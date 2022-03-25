c27edf08814518090ca44ac4885cf9862b6d9f59
Gwesa <gwesafumbuka@gmail.com>
2022-03-26 02:12:09 +0300

    accepted changes

--- smartbilling_source_code/editcategory2.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("connect.php");
-
-$sql="UPDATE categories  set name='".$_POST['name']."' where id = '".$_GET['id']."'";
-$result=$conn->query($sql);
-
-header('location:categories.php');
-?>
